86ebb0154e7fa254527e52ff01f0adcdb0ff764c
FND <fnd@localhost.localdomain>
2017-02-17 08:17:56 +0100

    switched to ES6 class syntax

--- src/checklist/item.js
+++ src/checklist/item.js
@@ -1,26 +1,28 @@
 import { createElement } from "../dom";
 
-export default function ChecklistItem(desc, done) {
-	this.desc = desc;
-	this.done = !!done;
+export default class ChecklistItem {
+	constructor(desc, done) {
+		this.desc = desc;
+		this.done = !!done;
+	}
+
+	static fromJSON(payload) {
+		return new this(payload.desc, payload.done);
+	}
+
+	render(tag) {
+		let cls = "checklist-item";
+		let params = {
+			class: this.done ? cls + " done" : cls,
+			"checklist-item": this // XXX: memory leak?
+		};
+
+		let dom = createElement;
+		return dom(tag, params, [
+			dom("label", null, [
+				dom("input", { type: "checkbox", checked: !!this.done }),
+				this.desc
+			])
+		]);
+	}
 }
-
-ChecklistItem.fromJSON = function(payload) {
-	return new this(payload.desc, payload.done);
-};
-
-ChecklistItem.prototype.render = function(tag) {
-	let cls = "checklist-item";
-	let params = {
-		class: this.done ? cls + " done" : cls,
-		"checklist-item": this // XXX: memory leak?
-	};
-
-	let dom = createElement;
-	return dom(tag, params, [
-		dom("label", null, [
-			dom("input", { type: "checkbox", checked: !!this.done }),
-			this.desc
-		])
-	]);
-};
--- src/store.js
+++ src/store.js
@@ -1,21 +1,23 @@
 import http, { assertStatus } from "./http";
 
-export default function Store(uri) {
-	this.uri = uri;
+export default class Store {
+	constructor(uri) {
+		this.uri = uri;
+	}
+
+	load() {
+		return http("GET", this.uri).
+			then(res => res.json()).
+			then(payload => {
+				this.data = payload;
+				return this.data;
+			});
+	}
+
+	save() {
+		return http("PUT", this.uri, null, JSON.stringify(this.data)).
+			then(res => {
+				assertStatus(res, [200, 201], "error saving");
+			});
+	}
 }
-
-Store.prototype.load = function() {
-	return http("GET", this.uri).
-		then(res => res.json()).
-		then(payload => {
-			this.data = payload;
-			return this.data;
-		});
-};
-
-Store.prototype.save = function() {
-	return http("PUT", this.uri, null, JSON.stringify(this.data)).
-		then(res => {
-			assertStatus(res, [200, 201], "error saving");
-		});
-};
